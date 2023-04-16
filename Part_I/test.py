7d6c72bab2f7eb1cb8ef72152103463d9ba82251
Daniil Kononov <k.d.va@yandex.ru>
2023-04-16 21:27:32 +0300

    add test module

---
+++ Part_I/test.py
@@ -0,0 +1,14 @@
+print("Hello, i'm a test module!")
+
+
+def plus(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print(plus(10, 4))
+    minus(10, 4)
