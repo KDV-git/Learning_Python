4b79f759698af7bd17a7b30b5a49ff0e0b95fd8a
Daniil Kononov <k.d.va@yandex.ru>
2023-04-20 04:17:35 +0300

    strings update

--- Part_II/strings.py
+++ Part_II/strings.py
@@ -18,3 +18,27 @@ print(type(byte_str))
 print(byte_str)
 
 print('\u00A3', '\u00A3'.encode('latin1'), b'\xA3'.decode('latin1'))
+
+print('\n---\n')
+
+x = ord('s')
+print(x)
+print(chr(x))
+print('\n')
+
+s = "3"
+for num in range(10):
+    s = chr(ord(s) + 1)
+    print(s)
+
+print('\n')
+
+# Преобразование двоичных чисел в целое число с помощью ord
+B = '1101'
+I = 0
+while B != '':
+    I = I * 2 + (ord(B[0]) - ord('0'))
+    B = B[1:]
+
+print(I)
+print(int('1101', 2))
