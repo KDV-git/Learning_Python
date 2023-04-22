074f4d705cab16631d49aae0faab9c62965dfd7a
Daniil Kononov <k.d.va@yandex.ru>
2023-04-23 02:38:41 +0300

    strings update (%, ".format()")

--- Part_II/strings.py
+++ Part_II/strings.py
@@ -42,3 +42,73 @@ while B != '':
 
 print(I)
 print(int('1101', 2))
+
+print('\n')
+
+# Примеры форматирования через ".format"
+template = '{0},{1} and {2}'
+print(template.format('spam', 'ham', 'eggs'))
+
+template = '{motto},{pork} and {food}'
+print(template.format(motto='spam', pork='ham', food='eggs'))
+
+template = '{motto},{0} and {food}'
+print(template.format('ham', motto='spam', food='eggs'))
+
+template = '{},{} and {}'
+print(template.format('spam', 'ham', 'eggs'))
+
+print('\n')
+
+# Добавление ключей, атрибутов и смещений
+import sys
+
+text = 'My {1[kind]} runs {0.platform}'.format(sys, {'kind': 'laptop'})
+print(text)
+
+text = 'My {map[kind]} runs {sys.platform}'.format(sys=sys, map={'kind': 'laptop'})
+print(text)
+
+somelist = list('SPAM')
+print(somelist)
+
+print('first={0[0]}, third={0[2]}'.format(somelist))
+
+print('first={0}, last={1}'.format(somelist[0], somelist[-1]))
+
+parts = somelist[0], somelist[-1], somelist[1:3]
+print(parts)
+
+print('first={0}, last={1}, middle={2}'.format(*parts))
+print('first={}, last={}, middle={}'.format(*parts))
+
+print('\n')
+
+# Расширенный синтаксис методов форматирования
+print('{0:10} = {1:10}'.format('spam', 123.4567))
+print('{:>10} = {:<10}'.format('spam', 123.4567))
+print('{0.platform:>10} = {1[kind]:<10}'.format(sys, dict(kind='laptop')))
+
+print('{0:e}, {1:.3e}, {2:g}'.format(3.14159, 3.14159, 3.14159))
+print('{0:f}, {1:.2f}, {2:06.2f}'.format(3.14159, 3.14159, 3.14159))
+
+print('\n')
+
+# Форматирования между системами счисления
+print('{0:x}, {1:o}, {2:b}'.format(255, 255, 255))
+print(bin(255), int('11111111', 2), 0b11111111)
+print(hex(255), int('FF', 16), 0xFF)
+print(oct(255), int('377', 8), 0o377)
+
+print('\n')
+
+# Строгое кодирование и сравнение с %
+print("{0:.2f}".format(1 / 3.0))
+print("%.2f" % (1 / 3.0))
+
+print('{0:.{1}f}'.format(1 / 3.0, 4))
+print('%.*f' % (4, 1 / 3.0))
+
+print('{0:.2f}'.format(1.2345))  # Строковый метод
+print(format(1.2345, '.2f'))  # Встроенная функция
+print('%.2f' % 1.2345)  # Выражение
