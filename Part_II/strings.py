8b5a03ba1aaddf1e617782762dcefe6107b7ae6a
Daniil Kononov <k.d.va@yandex.ru>
2023-04-23 23:01:15 +0300

    update last commit (%, ".format()")

--- Part_II/strings.py
+++ Part_II/strings.py
@@ -45,6 +45,40 @@ print(int('1101', 2))
 
 print('\n')
 
+# Примеры форматирования через %
+print('That is %d %s bird!' % (1, 'dead'))
+print('%d %s %g you' % (1, 'spam', 4.0))
+
+x = 1.23456789
+print('%e | %f | %g' % (x, x, x))
+print('%E' % x)
+
+print('%-6.2f | %05.2f | %+06.1f' % (x, x, x))
+print('%s' % x, str(x))
+
+print('%f, %.2f, %.*f' % (1 / 3.0, 1 / 3.0, 4, 1 / 3.0))
+
+print('\n')
+
+# Примеры форматирования через % со словарём
+print('%(qty)d more %(food)s' % {'qty': 1, 'food': 'spam'})
+
+reply = """
+Greetings...
+Hello %(name)s!
+Your age is %(age)s
+"""
+values = {'name': 'Bob', 'age': '40'}
+print(reply, values)
+print(reply % values)
+
+food = 'spam'
+qty = 10
+print(vars())
+print('%(qty)d more %(food)s' % vars())
+
+print('\n')
+
 # Примеры форматирования через ".format"
 template = '{0},{1} and {2}'
 print(template.format('spam', 'ham', 'eggs'))
