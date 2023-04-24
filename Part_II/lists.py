ebdbfbd417a8f27ba74d7b66c117cfeb4a6ec5fb
Daniil Kononov <k.d.va@yandex.ru>
2023-04-25 01:54:09 +0300

    lists completed

---
+++ Part_II/lists.py
@@ -0,0 +1,115 @@
+# Примеры работы со списками
+lst = [*range(-2, 3)]
+abs_lst = [abs(x) for x in lst]
+map_lst = list(map(abs, lst))
+print(lst)
+print(abs_lst)
+print(map_lst)
+
+print('\n')
+
+# Присваивания по индексам и срезам
+L = ['spam', 'Spam', 'SPAM!']
+print(L)
+L[0:2] = ['eat', 'more']
+print(L)
+
+print('\n')
+
+L = [1, 2, 3]
+print(L)
+L[1:2] = [4, 5]
+print(L)
+L[1:1] = [6, 7]
+print(L)
+
+print('\n')
+
+L = [0, 1]
+print(L)
+L[:0] = [2, 3, 4]
+print(L)
+
+print('\n')
+
+# Дополнительные сведения о сортировке списков
+L = ['abc', 'ABD', 'aBe']
+print(L)
+L.sort()
+print(L)
+
+print('\n')
+
+L = ['abc', 'ABD', 'aBe']
+print(L)
+L.sort(key=str.lower)
+print(L)
+
+print('\n')
+
+L = ['abc', 'ABD', 'aBe']
+print(L)
+L.sort(key=str.lower, reverse=True)
+print(L)
+
+print('\n')
+
+# Сортировка с помощью встроенной функции
+L = ['abc', 'ABD', 'aBe']
+print(f'L = {L}')
+sort_lst = sorted(L, key=str.lower, reverse=True)
+print(f'L = {L}')
+print(f'sort_lst = {sort_lst}')
+
+print('\n')
+
+L = ['abc', 'ABD', 'aBe']
+print(f'L = {L}')
+sort_lst = sorted([x.lower() for x in L], reverse=True)
+print(f'L = {L}')
+print(f'sort_lst = {sort_lst}')
+
+print('\n')
+
+# Примеры других списковых методов
+L = ['spam', 'eggs', 'ham']
+print(L)
+print(f'\'eggs\' index = {L.index("eggs")}')
+L.insert(1, 'toast')
+print(L)
+
+print('\n')
+
+L = ['spam', 'eggs', 'ham', 'toast']
+print(L)
+del L[0]
+print(L)
+del L[1:]
+print(L)
+
+print('\n')
+
+L = ['Already', 'got', 'one']
+L[1:] = []
+print(L)
+print(len(L))
+L[0:] = []
+print(L)
+print(len(L))
+
+print('\n')
+
+# Беглый взгляд на копирование списков
+L = [1, 2, 3, 4, 5]
+L2 = L.copy()
+L3 = L[:]
+L4 = list(L)
+
+print(L)
+print(L2)
+print(L3)
+print(L4)
+
+print(L2 is L)
+print(L3 is L)
+print(L4 is L)
