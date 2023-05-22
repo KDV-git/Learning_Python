b977175cb8a61f89a98d4db1d6f021bc91ba7390
Daniil Kononov <k.d.va@yandex.ru>
2023-05-22 15:22:01 +0300

    ch_XX_exercises_part_IV in progress_2

--- Part_IV/ch_XX_exercises_part_IV.py
+++ Part_IV/ch_XX_exercises_part_IV.py
@@ -25,13 +25,55 @@ print(adder('sp', 'am'))
 print(adder([1, 2], [3, 4]))
 print(adder(1.2, 3.4))
 
-adder(5, 10)
-adder('sp', 'am')
-adder([1, 2], [3, 4])
-adder(1.2, 3.4)
+print('\n')
+
 
 # 3. Переменное количество аргументов.
+def adder(*args):
+    res = args[0]
+    try:
+        for item in args[1:]:
+            res += item
+        return res
+    except TypeError:
+        return '\'TypeError: Unsupported operand type(s) for +=\''
+
+
+print(adder(5, 10, 15))
+print(adder('sp', 'am', '-', 'ham'))
+print(adder([1, 2], [3, 4], [5]))
+print(adder(1.2, 3.4, 5.6, 7.8, 9.1))
+print(adder(1.2, 1, 50))
+
+# Error
+print(adder(1.2, 's'))
+print(adder(10, [20, 30], '40'))
+print(adder({'a': 1, 'b': 2, 'c': 3}, {'e': 4, 'f': 5, 'g': 6}))
+print(adder({'a': 1, 'b': 2, 'c': 3}.values(), {'e': 4, 'f': 5, 'g': 6}.values()))
+
+print('\n')
+
+
 # 4. Ключевые аргументы.
+def adder(good=5, bad=2, ugly=3):
+    return good + bad + ugly
+
+
+print(adder())
+print(adder(ugly=1, good=2))
+print(adder(1, 2, 3))
+print(adder(1))
+print(adder('good', 'bad', 'ugly'))
+
+# print(adder(1, 2, 3, 4)) - TypeError: adder() takes from 0 to 3 positional arguments but 4 were given
+# print(adder('good', 'bad')) - TypeError: can only concatenate str (not "int") to str
+
+print()
+
+
+def adder(**kargs):
+    pass
+
 # 5. Словарные инструменты.
 # 6. Словарные инструменты.
 # 7. Дополнительные примеры сопоставления аргументов.
