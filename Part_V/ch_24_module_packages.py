1b26a8b6c8f09cf1944e7bbf21d222339363ae3a
Daniil Kononov <k.d.va@yandex.ru>
2023-05-27 14:59:32 +0300

    ch_24_module_packages in progress_2

--- Part_V/ch_24_module_packages.py
+++ Part_V/ch_24_module_packages.py
@@ -10,6 +10,7 @@ print(Part_IV.ch_17_1_global_scopes_test.var)
 
 # Пути к каталогам в сценарии после импорта становятся реальными цепочками вложенных объектов
 print(Part_IV)
+print(type(Part_IV))
 print(dir(Part_IV))
 
 # print(Part_I) - NameError: name 'Part_I' is not defined. Did you mean: 'Part_IV'?
@@ -17,3 +18,46 @@ print(dir(Part_IV))
 print('\n')
 
 # Пример импортирования пакетов
+import dir1.dir2.mod
+from importlib import reload
+
+reload(dir1)
+# reload(dir2) - NameError: name 'dir2' is not defined. Did you mean: 'dir1'?
+reload(dir1.dir2)
+# reload(mod) - NameError: name 'mod' is not defined
+reload(dir1.dir2.mod)
+
+print()
+
+print(dir1)
+print(dir1.dir2)
+print(dir1.dir2.mod)
+
+print()
+
+print(dir1.x)
+print(dir1.dir2.y)
+print(dir1.dir2.mod.z)
+
+# print(mod.z) - NameError: name 'mod' is not defined
+
+print('\n')
+
+# С пакетами часто удобнее использовать оператор from
+from dir1.dir2 import mod
+
+print(mod)
+print(mod.z)
+
+# Расширение import as также решает эту проблему
+import dir1.dir2.mod as modas
+
+print(modas)
+print(modas.z)
+
+from dir1.dir2 import mod as modfromas
+
+print(modfromas)
+print(modfromas.z)
+
+print('\n')
